88f2d4aebf511b59860e7dff0bc55b9a4f785dc4
yagocasas <yagocd10@hotmail.com>
2022-11-07 00:11:01 +0100

    ADDED button show/disguise experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Experience.scss";
+import { useState } from "react";
 
 const Experience = ({ experience }) => {
   // console.log('RETAIL', experience.retail);
@@ -10,31 +11,45 @@ const Experience = ({ experience }) => {
   const Sport = experience.sport;
   console.log("sports", Sport);
 
+  const [showExperience, setShowExperience] = useState(true);
+
   return (
     <div className="name">
       <h1>Experience</h1>
-      {Retail.map((element) => {
-        return (
-            <div key={JSON.stringify(element)}>
-              <h2 className="name">{element.name}</h2>
-              <h3>{element.where}</h3>
-              <p>{element.date}</p>
-              <p>{element.description}</p>
-            </div>
-    );
-  })}
-      {Sport.map((element) => {
-        return (
-            <div key={JSON.stringify(element)}>
-              <h2 className="name">{element.name}</h2>
-              <h3>{element.where}</h3>
-              <p>{element.date}</p>
-              <p>{element.description}</p>
-            </div>
-    );
-  })}
 
-  </div>
-  )};       
-             
+      <button onClick={() => setShowExperience(!showExperience)}>
+        {showExperience ? "Ocultar" : "Mostrar"} Experience
+      </button>
+
+      {showExperience 
+          ? <>
+          {Retail.map((element) => {
+            return (
+              <div key={JSON.stringify(element)}>
+                <h2 className="name">{element.name}</h2>
+                <h3>{element.where}</h3>
+                <p>{element.date}</p>
+                <p>{element.description}</p>
+              </div>
+            );
+          })};
+          {Sport.map((element) => {
+            return (
+              <div key={JSON.stringify(element)}>
+                <h2 className="name">{element.name}</h2>
+                <h3>{element.where}</h3>
+                <p>{element.date}</p>
+                <p>{element.description}</p>
+              </div>
+            );
+          })}
+          </> 
+          : <div>Experience Oculta</div>
+      }
+
+     
+    </div>
+  );
+};
+
 export default Experience;
